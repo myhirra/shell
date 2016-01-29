a9d19f80f59389fb755d752651a8ca571e699474
myhirra <myhirra@126.com>
2016-01-29 17:33:30 +0800

    修改组织方式，增加baidu、google等 hirra

--- alias.sh
+++ alias.sh
@@ -2,13 +2,5 @@
 
 path=$HOME'/.zshrc';
 
-aliases=(
-  "alias go=\"cd ~/projects/biz/\" "
-  "alias myip=\"ifconfig | grep 'inet ' | grep -v 127.0.0.1 | cut -d\ -f2\" "
-  "alias mypublicip=\"curl ifconfig.me\" "
-);
-
-for ((i=0;i<${#aliases[@]};i++));
-do
- echo ${aliases[i]} >> $path
-done
+echo '\n' >> $path
+cat ./alias.rc >> $path
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 #shell
 
 一些简单的命令、alias
+
+##alias
+
+如果你是zsh
+
+  sh alias.sh
